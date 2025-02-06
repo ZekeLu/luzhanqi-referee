ee45d5ab9e4b0a00377abe9f67a90c0299041cef
Zeke Lu <lvzecai@gmail.com>
2025-02-06 14:42:29 +0800

    modify the icons and name

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,22 @@ export default defineConfig({
     preact(),
     VitePWA({
       manifest: {
+        name: '军棋裁判',
         theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+        display: 'standalone',
       },
     }),
   ],
